591d06a61ea3c2cb010926c409c651d6d668e2e1
Naylin Medina <mejiasnam@gmail.com>
2020-08-01 19:09:32 -0400

    feat: add header component
    Signed-off-by: Naylin Medina <mejiasnam@gmail.com>

---
+++ src/Header.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render() {
+        return (
+            <header className="">
+                <div className="ui three item menu">
+                    <span className="item">Personal Challenge</span>
+                    <div className="ui floating labeled icon dropdown button item" id="categories">
+                        <i className="filter icon"></i>
+                        <span className="text">Filter</span>
+                        <div className="menu">
+                            <div className="item" data-value="all">
+                                All
+                            </div>
+                            <div className="item" data-value="important">
+                                Important
+                            </div>
+                            <div className="item" data-value="later">
+                                Later
+                            </div>
+                            <div className="item" data-value="tomorrow">
+                                Tomorrow
+                            </div>
+                        </div>
+                    </div>
+                    <a class="item" href="https://github.com/Naylin15/react-todolist">View it on GitHub</a>
+                </div>
+            </header>
+        )
+    }
+}
+
+export default Header

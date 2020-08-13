823a1c54aa914f32844753ccd3626307c7c68d6b
Naylin Medina <mejiasnam@gmail.com>
2020-08-13 18:11:40 -0400

    feat: remove state change class to function component
    Signed-off-by: Naylin Medina <mejiasnam@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import 'fomantic-ui-css/semantic.css';
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import Header from './Header';
 import AddNote from './AddNote';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {notes: []};
-  }
-  render() {
-    return (
-      <div className="App">
-        <Header />
-        <AddNote notesArray={this.state.notes} />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <AddNote />
+    </div>
+  );
 }
 
 export default App;

5ea72e4e670c0746df6de8f27a33c81fa47ec0be
Naylin Medina <mejiasnam@gmail.com>
2020-09-03 21:26:32 -0400

    feat: remove dropdown component
    Signed-off-by: Naylin Medina <mejiasnam@gmail.com>

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,18 +6,8 @@ class Header extends Component {
     render() {
         return (
             <header className="main-header">
-                <div className="ui three item menu">
+                <div className="ui two item menu">
                     <span className="item">Personal Challenge</span>
-                    <div className="ui floating labeled icon dropdown button item" id="categories">
-                        <Dropdown text='Filter by categories'>
-                            <Dropdown.Menu>
-                                <Dropdown.Item text='All'/>
-                                <Dropdown.Item text='Important' />
-                                <Dropdown.Item text='Tomorrow' />
-                                <Dropdown.Item text='Later' />
-                            </Dropdown.Menu>
-                        </Dropdown>
-                    </div>
                     <a className="item" href="https://github.com/Naylin15/react-todolist">View it on GitHub</a>
                 </div>
             </header>
